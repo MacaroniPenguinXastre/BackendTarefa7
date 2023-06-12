5afb685d9f9f3a2b9cc19dd51c5ca0c593dcbef3
marcato10 <lucasmarcato10@hotmail.com>
2023-06-12 18:05:28 -0300

    F

--- src/test/java/com/macaroni/projectonlinestudent/ProjectOnlineStudentApplicationTests.java
+++ src/test/java/com/macaroni/projectonlinestudent/ProjectOnlineStudentApplicationTests.java
@@ -1,13 +1,41 @@
 package com.macaroni.projectonlinestudent;
 
+import com.macaroni.projectonlinestudent.DTO.LoginDTO;
+import com.macaroni.projectonlinestudent.Model.User;
+import com.macaroni.projectonlinestudent.Repository.UserRepository;
+import com.macaroni.projectonlinestudent.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @SpringBootTest
 class ProjectOnlineStudentApplicationTests {
 
+    @Autowired
+    private SecurityConfig securityConfig;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
-    void contextLoads() {
+    void testLoginwithEmailAndPassword(LoginDTO loginDTO){
+        try{
+            User logginUser = userRepository.findUserByEmail(loginDTO.email());
+            assertNull(logginUser,"Existe um email?");
+
+            boolean result = securityConfig.passwordEncoder().matches(loginDTO.password(),logginUser.getPassword());
+            assertTrue("Senha correta?",result);
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+        }
     }
 
 }
